d41bb8c43c7612216b3b2836c0abb05028234f48
IlyaSavchenko <sav.il.yu@gmail.com>
2013-10-27 16:00:03 +0400

    addPort

--- server.c
+++ server.c
@@ -5,7 +5,26 @@
 #include<sys/socket.h>
 
 
-main(){
+main(int argc, char* argv[]){
+	char *USER;
 	struct sockaddr_in clientaddr; //sockaddr — обобщённая структура адреса, к которой, в зависимости от используемого семейства протоколов, приводится соответствующая структура -> Wiki
     socklen_t addrlen; // Представляет длину структуры sockaddr -> Wiki
+    char abr;	
+    char PORT[4]; 
+    strcpy(PORT,"4444");
+
+    if ((abr = getopt(argc, argv, "p:u:")) < -1)
+        switch (abr)
+        {
+            case 'u':
+                USER = malloc(strlen(optarg));
+                strcpy(USER,optarg);
+                break;
+            case 'p':
+                strcpy(PORT,optarg);
+                break;
+            default:
+                exit(1);
+        }
+      
 }
