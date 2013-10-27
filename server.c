982ce8a39b6f37770608f8dbb81f2321da2f8440
IlyaSavchenko <sav.il.yu@gmail.com>
2013-10-27 18:45:41 +0400

    add_Accept_Function_ForWork_With_Socket

--- server.c
+++ server.c
@@ -7,6 +7,7 @@
 
 main(int argc, char* argv[]){
 	char *USER;
+	int clients[100];
 	struct sockaddr_in clientaddr; //sockaddr — обобщённая структура адреса, к которой, в зависимости от используемого семейства протоколов, приводится соответствующая структура -> Wiki
     socklen_t addrlen; // Представляет длину структуры sockaddr -> Wiki
     char abr;	
@@ -23,8 +24,27 @@ main(int argc, char* argv[]){
             case 'p':
                 strcpy(PORT,optarg);
                 break;
+            case '?':
+                fprintf(stderr,"Bad arguments!\n");
+                exit(1);
             default:
                 exit(1);
         }
+
+        int con = 0;
+        while (1)
+    	{
+        	addrlen = sizeof(clientaddr);
+        	clients[con] = accept(listenfd, (struct sockaddr *) &clientaddr, &addrlen);
+
+        	if (clients[con] < 0)
+            	error ("accept() error");
+
+        	while (clients[slot] != -1) 
+        		slot = (slot + 1) % CONNMAX;
+    	}
+
+    	return 0;
+}
       
 }
