48d4edf0fe4bf16eed0ec552e057c468abe6789a
IlyaSavchenko <sav.il.yu@gmail.com>
2013-10-27 15:39:36 +0400

    startWork

--- server.c
+++ server.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<sys/socket.h>
+
+
 main(){
-	printf("This module for server\n");
+	struct sockaddr_in clientaddr; //sockaddr — обобщённая структура адреса, к которой, в зависимости от используемого семейства протоколов, приводится соответствующая структура -> Wiki
+    socklen_t addrlen; // Представляет длину структуры sockaddr -> Wiki
 }
