15cd469d3aeb9b6d9aa47aeb5b9813c6e66b3106
IlyaSavchenko <sav.il.yu@gmail.com>
2013-11-04 22:46:49 +0400

    eee... git defeated. go go go

--- server.c
+++ server.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
 
 main{
+	char *CLIENT;
+	struct sockaddr_in clientaddr;
+    socklen_t addrlen;
+    char PORT[4];
+    CLIENT = getenv("PWD");
+    strcpy(PORT,"9080");
+
+    int i;
+    for (i = 0; i < 1000; i++)
+        clients[i] = -1;
+    startServer(PORT);
+   
+    return 0;
+}
 	
 }
